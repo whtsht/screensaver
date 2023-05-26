9b9406ebd2c35aae2233bc63f48a546d35aef329
whtsht <whiteshirt0079@gmail.com>
2023-05-26 15:57:00 +0900

    split file contents

---
+++ inc/token.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {
+    TK_NUM,
+    TK_ADD,
+    TK_SUB,
+    TK_MUL,
+    TK_DIV,
+    TK_MOD,
+    TK_RPA,
+    TK_LPA,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token* next;
+    char* string;
+};
+
+typedef struct {
+    char* string;
+    int current;
+    int size;
+} Stream;
+
+Stream* to_stream(FILE* fp);
+
+Token* tokenize(Stream* stream);
