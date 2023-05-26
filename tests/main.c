70a5e26396a2f774216338fb7be3357e181d5839
whtsht <whiteshirt0079@gmail.com>
2023-05-27 00:20:29 +0900

    add token tests

--- tests/main.c
+++ tests/main.c
@@ -5,7 +5,13 @@
 
 #include "../inc/token.h"
 
+void test_token();
+
 int main() {
+    test_token();
+}
+
+void test_token() {
     FILE* fp;
     fp = fopen("tests/simple.tur", "r");
     Stream* stream = to_stream(fp);
@@ -14,4 +20,6 @@ int main() {
     free_stream(stream);
 
     assert(token->kind == TK_IDT);
+    assert(token->next->kind == TK__EQ);
+    assert(token->next->next->kind == TK_NUM);
 }
