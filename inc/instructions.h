c5424a14da5ba62f3230d4b25db986653642bb26
whtsht <whiteshirt0079@gmail.com>
2023-05-26 18:43:16 +0900

    add instructions

---
+++ inc/instructions.h
@@ -0,0 +1,26 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include "node.h"
+
+typedef enum {
+    // A:
+    IN_LABEL,
+    // goto A
+    IN_GOTO,
+    // cgoto A a == b
+    IN_COND_GOTO,
+    // forward(10)
+    IN_FUNC,
+    // val = 5
+    IN_ASSIGN,
+} InstrKind;
+
+typedef struct Instr Instr;
+
+struct Instr {
+    InstrKind kind;
+    Node** nodes;
+};
+
+#endif
